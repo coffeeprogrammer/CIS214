2779358ba99f70baece1138664a1e77813d53135
Chad Botting <chad_b14@yahoo.com>
2012-09-05 21:47:03 -0400

    test second commit

---
+++ Module_01/HelloWorld.java
@@ -0,0 +1,15 @@
+// My first Java application
+// Author:  Chad Botting
+// Course:  CIS 214, fall 2012
+// Date:    August 21, 2012
+// Classic Hello World program in Java
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("Hello World");
+
+	} // end of main method
+} // end of HelloWorld class
